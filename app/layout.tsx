7d260388cbd448351a060f24be56f765f91a75c8
panda-1127 <lucaschang1127@gmail.com>
2026-03-04 00:35:27 +0800

    version3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next"
 import { Inter, Noto_Sans_SC } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import { ThemeProvider } from "next-themes"
 import { LanguageProvider } from "@/lib/language-context"
 import "./globals.css"
@@ -41,7 +40,6 @@ export default function RootLayout({
           </LanguageProvider>
         </ThemeProvider>
         <Analytics />
-        <SpeedInsights />
       </body>
     </html>
   )
